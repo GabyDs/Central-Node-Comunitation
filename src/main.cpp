da09d4c2932cce01e55d45fa379b5abf446be14a
GabyDs <gabydsch2001@gmail.com>
2025-08-27 17:44:05 -0300

    Setup ESP as access point

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include <Arduino.h>
+#include <WiFi.h>
+
+const char* ssid     = "FIOUNAMBOTS";
+const char* password = "fiounambots2025";
+
+IPAddress local_IP(192,168,5,1);
+IPAddress gateway(192,168,5,1);
+IPAddress subnet(255,255,255,0);
 
 void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password); //Start Acces point mode
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  Serial.println("Access Point Started");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+  
 }
 
 void loop() {
